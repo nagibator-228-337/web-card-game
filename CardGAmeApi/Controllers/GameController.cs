ad6af4920567947c7a363428be462df378a0be22
ILeshiy <153270138+ILeshiy@users.noreply.github.com>
2025-04-09 00:39:20 -0700

    Yes

--- CardGAmeApi/Controllers/GameController.cs
+++ CardGAmeApi/Controllers/GameController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using CardGameCore.Mechanics;
 
 namespace CardGameApi.Controllers
 {
@@ -6,10 +7,26 @@ namespace CardGameApi.Controllers
     [Route("api/[controller]")]
     public class GameController : ControllerBase
     {
+        private Game _game;
+
+        public GameController()
+        {
+ 
+            _game = new Game(2); 
+        }
+
+        [HttpGet("start")]
+        public IActionResult StartGame()
+        {
+            _game.StartGame();
+            return Ok("Game started");
+        }
+
         [HttpGet("status")]
         public IActionResult GetStatus()
         {
-            return Ok(new { message = "Game was laucnhed" });
+            
+            return Ok(new { Player1 = _game.Players[0].Score, Player2 = _game.Players[1].Score });
         }
     }
 }
